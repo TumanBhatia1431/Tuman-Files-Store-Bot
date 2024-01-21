3fa4c7929b5aee61ae113d1c03deba582164efde
TumanBhatia1431 <157300467+TumanBhatia1431@users.noreply.github.com>
2024-01-21 21:39:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
-cd boted;
+git clone https://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@github.com/TumanFiles/Tuman-Files-Store-Bot
+cd boted;/Tuman-Files-Store-Bot
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
 python3 bot.py;
